c098340ba864f02481dcdcc6371d52547392965a
Dennie Froese <dennie.froese@santander.co.uk>
2020-06-10 20:33:48 +0100

    added useEffect to scatter component to map data and select all g elements to render

--- src/components/Scatter/index.js
+++ src/components/Scatter/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { select, scaleLinear, extent } from "d3";
 
 function RandomData() {
   const data = [...Array(100)].map((e, i) => {
@@ -12,6 +13,8 @@ function RandomData() {
 }
 
 function Scatter() {
+  const ref = useRef(null);
+
   const data = RandomData();
   const w = 600;
   const h = 600;
@@ -25,10 +28,31 @@ function Scatter() {
   const width = w - margin.right - margin.left;
   const height = h - margin.top - margin.bottom;
 
+  useEffect(() => {
+    const g = select(ref.current);
+
+    const xScale = scaleLinear()
+      .domain(extent(data, d => d.x))
+      .range([0, width]);
+
+    const yScale = scaleLinear()
+      .domain(extent(data, d => d.y))
+      .range([height, 0]);
+
+    g.selectAll(".circles")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("r", 3)
+      .attr("cx", d => xScale(d.x))
+      .attr("cy", d => yScale(d.y))
+      .attr("fill", "black");
+  }, [data, height, width]);
+
   return (
     <div>
       <svg width={w} height={h}>
-        <g transform={`translate(${margin.left},${margin.top})`}></g>
+        <g ref={ref} transform={`translate(${margin.left},${margin.top})`}></g>
       </svg>
     </div>
   );
